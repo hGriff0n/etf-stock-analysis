5dde24a69eaedd1abe1a4e711edf80b767895055
hGriff0n <ghooper96@gmail.com>
2020-07-18 20:47:30 -0400

    Tidy-up HoldingList Styling

--- src/app/core/components/stocks/holding/holding.component.ts
+++ src/app/core/components/stocks/holding/holding.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 // An individual stock holding for the portfolio page. Expandable to per-brokerage holdings
 // Has up/down integration with a planning service (and/or internal count)
@@ -9,33 +9,46 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './holding.component.html',
   styleUrls: ['./holding.component.scss']
 })
-export class StockHoldingComponent {
+export class StockHoldingComponent implements OnInit {
 
 
   @Input()
   symbol = "";
+  // TODO: These will likely be produced from SecuritydataService
+  @Input()
   name = "Ford";
-
-  quantity = 5;
-
+  @Input()
+  held = 5;
+  @Input()
   price = 24.53;
 
+  quantity = 0;
+  equity = 0;
+
+  // TODO: Eventually surface from summation of sub-components?
   brokerages = {
     robinhood: {
-      gain_loss_pct: 24.2,
       held_shares: 3
     },
     schwab:  {
-      gain_loss_pct: 4.2,
       held_shares: 2
     }
   }
 
-  constructor() { }
+  ngOnInit() {
+    this.quantity = this.held;
+    this.equity = this.held * this.price;
+    this.change.emit({
+      symbol: this.symbol,
+      delta: this.equity
+    })
+  }
 
   onChange(event) {
     this.brokerages[event.broker].held_shares += event.delta;
     this.quantity += event.delta;
+    this.equity += this.price * event.delta;
+
     this.change.emit({
       symbol: this.symbol,
       delta: event.delta * this.price
